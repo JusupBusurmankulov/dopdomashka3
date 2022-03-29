5b604d96a8dd17ed14a20b58d09240eded22b94a
JusupBusurmankulov <zusupbusurmankulov@gmail.com>
2022-03-29 15:44:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Main {
+
+    static void bubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
+    }
+    static void printArray(int arr[], int size)
+    {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] array = {4, 5, 1, 7, 8, 3, 2, 6, 9, 10};
+        int n = array.length;
+        bubbleSort(array, n);
+        System.out.println("Sorted array: ");
+        printArray(array, n);
+    }
+}
